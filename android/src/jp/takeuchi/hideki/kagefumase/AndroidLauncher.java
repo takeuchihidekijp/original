34fe7d493edd5187539690d9c8e6d962eb3c7516
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-02-22 23:33:37 +0900

    Initial commit

---
+++ android/src/jp/takeuchi/hideki/kagefumase/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package jp.takeuchi.hideki.kagefumase;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jp.takeuchi.hideki.kagefumase.Kagefumase;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Kagefumase(), config);
+	}
+}
---
+++ core/src/jp/takeuchi/hideki/kagefumase/GameScreen.java
@@ -0,0 +1,16 @@
+package jp.takeuchi.hideki.kagefumase;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by OWNER on 2017/02/22.
+ */
+
+public class GameScreen extends ScreenAdapter {
+    private Kagefumase mGame;
+
+    public GameScreen(Kagefumase game){
+        mGame = game;
+    }
+}
