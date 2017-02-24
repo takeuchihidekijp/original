066ba76544b0d619ace457168f535430e80bdd26
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-02-24 09:45:32 +0900

    TO DO 画像の準備　各オブジェクトの作成

--- core/src/jp/takeuchi/hideki/kagefumase/GameScreen.java
+++ core/src/jp/takeuchi/hideki/kagefumase/GameScreen.java
@@ -1,16 +1,61 @@
 package jp.takeuchi.hideki.kagefumase;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
  * Created by OWNER on 2017/02/22.
  */
 
 public class GameScreen extends ScreenAdapter {
+
+    static final float CAMERA_WIDTH = 10;
+    static final float CAMERA_HEIGHT = 15;
+
     private Kagefumase mGame;
+
+    Sprite mBg;
+    OrthographicCamera mCamera;
+    FitViewport mViewPort;
 
     public GameScreen(Kagefumase game){
         mGame = game;
+
+        // TODO 背景の準備 まだ画像を用意できず
+
+        // カメラ、ViewPortを生成、設定する
+        mCamera = new OrthographicCamera();
+        mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+        mViewPort = new FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT, mCamera);
+
     }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // カメラの座標をアップデート（計算）し、スプライトの表示に反映させる
+        mCamera.update();
+        mGame.batch.setProjectionMatrix(mCamera.combined);
+
+        mGame.batch.begin();
+
+        // 原点は左下
+        mBg.setPosition(mCamera.position.x - CAMERA_WIDTH / 2, mCamera.position.y - CAMERA_HEIGHT / 2);
+        mBg.draw(mGame.batch);
+
+        mGame.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        mViewPort.update(width, height);
+    }
+    
 }
